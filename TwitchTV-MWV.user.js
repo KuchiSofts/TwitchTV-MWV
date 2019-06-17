6c2c7f962cc7aaa49566780769df9fe997ca8f0b
KuchiSofts <32385966+KuchiSofts@users.noreply.github.com>
2019-06-17 03:45:33 +0300

    Update TwitchTV-MWV.user.js

--- TwitchTV-MWV.user.js
+++ TwitchTV-MWV.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
-// @name         TwitchTV-MWV
-// @namespace    http://kuchi.be/
-// @version      1.2
-// @description  Control TwitchTV volume by scroll mouse wheel up and down with saving the volume settings by Kuchi - Soft's
-// @author       Kuchi - Soft's
+// @name            TwitchTV-MWV
+// @namespace       http://kuchi.be/
+// @version         1.3
+// @description     Control TwitchTV volume by scroll mouse wheel up and down with saving the volume settings by Kuchi - Soft's
+// @author          Kuchi - Soft's
+// @defaulticon     https://github.com/KuchiSofts/TwitchTV-MWV/raw/master/TwitchTV-MWV-icon.png
 // @icon            https://github.com/KuchiSofts/TwitchTV-MWV/raw/master/TwitchTV-MWV-icon.png
-// @updateURL    https://github.com/KuchiSofts/TwitchTV-MWV/raw/master/TwitchTV-MWV.user.js
-// @downloadURL  https://github.com/KuchiSofts/TwitchTV-MWV/raw/master/TwitchTV-MWV.user.js
-// @match        *://*.twitch.tv/*
-// @match        *://*.twitch.tv/videos/*
-// @run-at      document-start
-// @grant        none
+// @updateURL       https://github.com/KuchiSofts/TwitchTV-MWV/raw/master/TwitchTV-MWV.user.js
+// @downloadURL     https://github.com/KuchiSofts/TwitchTV-MWV/raw/master/TwitchTV-MWV.user.js
+// @match           *://*.twitch.tv/*
+// @match           *://*.twitch.tv/videos/*
+// @run-at          document-start
+// @grant           none
 // @priority        9000
 // ==/UserScript==
 
